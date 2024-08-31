f1b5102ff150f51d378897b2421cc07c3ea78147
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-08-31 11:19:28 -0300

    feat: add migration with flyway for populate database

---
+++ src/main/java/com/example/exportpdfexample/FlywayConfig.java
@@ -0,0 +1,20 @@
+package com.example.exportpdfexample;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+
+    @Autowired
+    public void migrateFlyway(DataSource dataSource) {
+        Flyway.configure()
+                .baselineOnMigrate(true)
+                .dataSource(dataSource)
+                .load()
+                .migrate();
+    }
+}
---
+++ src/main/resources/db/migration/V2__populate-tables.sql
@@ -0,0 +1,7 @@
+INSERT INTO analysis (title, description) VALUES ('Analysis 1', 'Description of the analysis 1');
+INSERT INTO analysis (title, description) VALUES ('Analysis 2', 'Description of the analysis 2');
+INSERT INTO analysis (title, description) VALUES ('Analysis 3', 'Description of the analysis 3');
+INSERT INTO analysis (title, description) VALUES ('Analysis 4', 'Description of the analysis 4');
+INSERT INTO analysis (title, description) VALUES ('Analysis 5', 'Description of the analysis 5');
+INSERT INTO analysis (title, description) VALUES ('Analysis 6', 'Description of the analysis 6');
+INSERT INTO analysis (title, description) VALUES ('Analysis 7', 'Description of the analysis 7');
